ecfd76446b38c0927bb73b656371811c70e2c784
Markosz <markosz@MacBook-Pro.local>
2026-06-01 12:33:38 +0200

    feat: import local ssh key dynamically via terraform

--- main.tf
+++ main.tf
@@ -11,7 +11,13 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-# 1. AWS Biztonsági Csoport (Tűzfal az Ubuntu gépnek)
+# 1. GENERÁLT KULCSPÁR IMPORTÁLÁSA (Ez garantálja az egyezést!)
+resource "aws_key_pair" "jenkins_mac_key" {
+  key_name   = "openstack-lab-key-dynamic"
+  public_key = file("/Users/markosz/.ssh/id_rsa.pub") # Beolvassa a Mac-ed publikus kulcsát
+}
+
+# 2. AWS Biztonsági Csoport
 resource "aws_security_group" "openstack_host_sg" {
   name        = "openstack-host-sg"
   description = "Allow SSH and OpenStack Horizon dashboard"
@@ -20,7 +26,7 @@ resource "aws_security_group" "openstack_host_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Élesben ide a saját IP-d jönne
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -31,14 +37,14 @@ resource "aws_security_group" "openstack_host_sg" {
   }
 }
 
-# 2. Az Ubuntu 22.04 LTS gép elindítása (t3.large, 30GB gp3)
+# 3. Az Ubuntu gép indítása az ÚJ kulccsal
 resource "aws_instance" "openstack_host" {
-  ami           = "ami-0c905937c14bd22b0" # A legutóbb sikeresen letesztelt AMI ID-d
+  ami           = "ami-0c905937c14bd22b0"
   instance_type = "t3.large"
   security_groups = [aws_security_group.openstack_host_sg.name]
   
-  // Feltételezzük, hogy a Jenkins szerveren a ~/.ssh/id_rsa.pub kulcsot használod
-  key_name = "openstack-lab-key" 
+  # Itt már a dinamikusan importált kulcsra hivatkozunk:
+  key_name = aws_key_pair.jenkins_mac_key.key_name 
 
   root_block_device {
     volume_size = 30
@@ -50,7 +56,6 @@ resource "aws_instance" "openstack_host" {
   }
 }
 
-# KIMENET: Ezt olvassa ki a Jenkins az Ansible számára
 output "ec2_public_ip" {
   value       = aws_instance.openstack_host.public_ip
   description = "Az AWS gép nyilvános IP címe"
